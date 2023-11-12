76dedf547f1fd668aba72ab59cd70efb489ab5f7
corinahincu <hincucorina97@gmail.com>
2023-11-12 02:36:11 +0200

    list-items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
-import { SplitScreen } from "./SplitScreen";
+import { RegularList } from "./RegularList";
+import { SmallPersonListItem } from "./people/SmallPersonListItem";
+import {LargePersonListItem} from "./people/LargePersonListItem"
+import {SmallProductListItem} from "./products/SmallProductListItem"
+import {LargeProductListItem} from "./products/LargeProductListItem"
+import { NumberedList } from "./NumberedList";
+/* import { Modal } from "./Modal"; */
 
 
-const LeftHandComponent = ({name}) => {
-  return <h1 style={{backgroundColor: ' green '}}>{name}</h1>
-}
-const RightHandComponent = ({message}) => {
-  return <h1 style={{backgroundColor:' red '}}>{message}</h1>
-}
+const people = [
+  {
+    name: "John Doe",
+    age: 54,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  },
+  {
+    name: "Brenda Smith",
+    age: 33,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  },
+];
+
+const products = [
+  {
+    name: "Flat-screen TV",
+    price: "$300",
+    description: "Hugo LCD screen, a great deal",
+    rating: 4.5,
+  },
+  {
+    name: "Basketball",
+    price: "$10",
+    description: "Just like the pros use",
+    rating: 3.8,
+  },
+];
 
 function App() {
   return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      < LeftHandComponent name="Shaun" />
-      < RightHandComponent message="hello"/>
-    </SplitScreen>
+    <>
+      <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem}
+      />
+      <NumberedList
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+      />
+
+      <RegularList
+        items={products}
+        resourceName="product"
+        itemComponent={SmallProductListItem}
+      />
+      <NumberedList
+        items={products}
+        resourceName="product"
+        itemComponent={LargeProductListItem}
+      /> 
+    </>
   );
 }
 
