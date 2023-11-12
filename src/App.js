a0fc1d386e1f94fcd3e1fef9fccb6fd5fdb901e1
corinahincu <hincucorina97@gmail.com>
2023-11-12 03:50:14 +0200

    uncontrolled form

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-import { RegularList } from "./RegularList";
-import { SmallPersonListItem } from "./people/SmallPersonListItem";
-import {LargePersonListItem} from "./people/LargePersonListItem"
-import {SmallProductListItem} from "./products/SmallProductListItem"
-import {LargeProductListItem} from "./products/LargeProductListItem"
-import { NumberedList } from "./NumberedList";
-import { Modal } from "./Modal";
-
-
-const people = [
-  {
-    name: "John Doe",
-    age: 54,
-    hairColor: "brown",
-    hobbies: ["swimming", "bicycling", "video games"],
-  },
-  {
-    name: "Brenda Smith",
-    age: 33,
-    hairColor: "brown",
-    hobbies: ["swimming", "bicycling", "video games"],
-  },
-];
-
-const products = [
-  {
-    name: "Flat-screen TV",
-    price: "$300",
-    description: "Hugo LCD screen, a great deal",
-    rating: 4.5,
-  },
-  {
-    name: "Basketball",
-    price: "$10",
-    description: "Just like the pros use",
-    rating: 3.8,
-  },
-];
+import { UncontrolledForm } from "./UncontrolledForm";
 
 function App() {
   return (
-    <>
-      <Modal>
-        <LargeProductListItem product={products[0]} />
-      </Modal>
-    </>
+    <UncontrolledForm/>
   );
 }
 
