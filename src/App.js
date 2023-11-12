0edec37870a8674416a11c8d29db2e91fd8ce7ac
corinahincu <hincucorina97@gmail.com>
2023-11-12 19:48:29 +0200

    controlled uncontrolled modals

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import { ControlledForm } from "./ControlledForm";
+/* import { ControlledForm } from "./ControlledForm"; */
+import { useState } from "react";
+import { ControlledModal } from "./ControlledModal";
 
 function App() {
+  const [shouldShowModal, setShouldShowModal] = useState(false)
   return (
-    <ControlledForm/>
+    <>
+      <ControlledModal 
+        shouldShow={shouldShowModal}
+        onRequestClose={()=> 
+          setShouldShowModal(false)}
+      >
+          <h1>Hello</h1>
+      </ControlledModal>
+      <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+        {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
+      </button>
+    </>
   );
 }
 
